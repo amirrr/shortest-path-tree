adabc6887787fbeb8183b174c9d7f10c190ac06c
Amir Nakhaei <>
2019-05-04 23:15:28 +0430

    node can creat their own children TODO: need a loop to make the tree from mother node

--- prototyping-python-ideas.py
+++ prototyping-python-ideas.py
@@ -1,4 +1,3 @@
-
 def checks(di, vis):
     res = set()
     for i in di:
@@ -13,10 +12,36 @@ def checks(di, vis):
 a = {1: 4, 2: 5, 3: 6}
 b = {}
 
-for key in a:
-    b[a[key]] = a[key]
-
-#a.update(b)
-
 print(a)
 print(checks(a, set([1,3])))
+
+class Abasi:
+    general = 0
+    def __init__(self, data):
+        self.d = data
+        Abasi.general = Abasi.general+1
+    
+    def get_data(self):
+        return self.d
+
+sall = []
+vall = []
+
+Abasi.general = 5
+
+a1 = Abasi(1)
+sall.append(a1)
+a2 = Abasi(2)
+sall.append(a2)
+a3 = Abasi(3)
+sall.append(a3)
+a4 = Abasi(4)
+sall.append(a4)
+a5 = Abasi(5)
+sall.append(a5)
+
+middle = int(len(sall)/2)+1
+print(middle)
+for i in sall:
+    print(i.d, '- ', end='')
+print('\n')
--- test.py
+++ test.py
@@ -10,16 +10,44 @@ import numpy as np
 
 class Node(object):
     """Return the square root of self times self."""
-
-    def __init__(self, data, vtd):
+    source_dest = {}
+    distance_matix = []
+    def __init__(self, data, vtd, distance):
         self.data = data
         self.children = []
         self.visited = set()
         self.visited.update(vtd)
+        self.distance = distance
+
+    def create_children(self):
+        """ creat the node children and return them if any, false otherwise """
+        univisited_set = possible_nodes(Node.source_dest, self.visited)
+        if len(univisited_set) != 0:
+            for i in univisited_set:
+                temp_visited_set = set(self.visited)
+                temp_visited_set.add(i)
+                temp_distance = self.distance + Node.distance_matix[self.data-1][i-1]
+                node = Node(i, temp_visited_set, temp_distance)
+                self.children.append(node)
+            return self.children
+        else:
+            return False
+
 
     def add_child(self, obj):
         """ add new node to list of children """
         self.children.append(obj)
+
+    def get_child(self):
+        """ return this nodes children """
+        return self.children
+
+    def print_data(self):
+        """ print list of children """
+        print('mother:', self.data, '----> ', end='')
+        for child in self.children:
+            print(child.data, end=' ')
+        print('')
 
 def possible_nodes(start_finish, visited):
     """ look for possible node expantion based on visited nodes """
@@ -49,6 +77,28 @@ def main():
 
     print(np.matrix(matris))
     # print()
+
+    Node.source_dest = origin_destination
+    Node.distance_matix = matris
+
+    mother_node = Node(0, [], 0)
+    c1 = Node(1, [1], 10)
+    c2 = Node(2, [2], 10)
+    c3 = Node(3, [3], 10)
+    mother_node.add_child(c1)
+    mother_node.add_child(c2)
+    mother_node.add_child(c3)
+    mother_node.print_data()
+
+    #print(possible_nodes(origin_destination, c1.visited))
+
+    c1child = c1.create_children()
+    c1.print_data()
+
+    for i in c1child:
+        i.create_children()
+        i.print_data()
+
 
 if __name__ == "__main__":
     main()
